37d0d34c1086f3243530afebb2bc177ba79fa9b4
unknown <haveri321@gmail.com>
2024-05-31 07:48:55 +0300

    added comment to members page; this is probably the way to fetch in next 14!

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -5,11 +5,12 @@ import { Member } from "@prisma/client";
 interface MembersPageProps {
   members: Array<Member>;
 }
+
 export const MembersPage: FC<MembersPageProps> = ({ members }) => {
   return (
     <div>
       <ul>
-        {members?.map((member) => {
+        {members.map((member) => {
           return <li key={member.id}>{member.name}</li>;
         })}
       </ul>
@@ -18,6 +19,8 @@ export const MembersPage: FC<MembersPageProps> = ({ members }) => {
 };
 
 // We can name the function MembersPage however we want (it's default), but that's his convention
+// I think now we don't need to use getServerSideProps, we can just do it in the component itself:
+// https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration#step-6-migrating-data-fetching-methods
 export default async function Page() {
   const members = await getMembers();
 
