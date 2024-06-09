f47160741165acf2b3b7fa686b0d3c4484fa355b
unknown <haveri321@gmail.com>
2024-06-09 09:34:47 +0300

    WIP creating the messages page, with inbox and outbox

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { MessageSideBar } from "./MessageSidebar";
 
 export default function MessagesPage() {
-  return <div>messages</div>;
+  return (
+    <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
+      <div className="col-span-2">
+        <MessageSideBar />
+      </div>
+      <div className="col-span-10">Message table goes here</div>
+    </div>
+  );
 }
