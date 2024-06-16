9fd0814895b394103d6e27f5c1afb1a6652d6065
unknown <haveri321@gmail.com>
2024-06-16 08:54:34 +0300

    created the messages store for notifications

---
+++ src/hooks/useMessagesStore.ts
@@ -0,0 +1,26 @@
+import { MessageDto } from "@/types";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type MessageState = {
+  messages: MessageDto[];
+  add: (message: MessageDto) => void;
+  remove: (id: MessageDto["id"]) => void;
+  set: (messages: MessageDto[]) => void;
+};
+
+const useMessagesStore = create<MessageState>()(
+  devtools(
+    (set) => ({
+      messages: [],
+      add: (message) =>
+        set((state) => ({ messages: [message, ...state.messages] })),
+      remove: (id) =>
+        set((state) => ({
+          messages: state.messages.filter((m) => m.id !== id),
+        })),
+      set: (messages) => set({ messages }),
+    }),
+    { name: "messagesStoreDemo" }
+  )
+);
