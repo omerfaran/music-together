8f755a9b4de4d5c25c7baebe1df96376b062e604
unknown <haveri321@gmail.com>
2024-06-30 10:46:26 +0300

    added Google provider; BUG: re-sign-in after profile complete makes google ask again for the email and everything

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -10,6 +10,10 @@ import { compare } from "bcryptjs";
 export default {
   // providers: [GitHub],
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
